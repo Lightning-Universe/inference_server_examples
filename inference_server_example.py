b80364b0a87f30fa5c79e215b995c73a149d5b10
Dmitry Frolov <dmitsf@gmail.com>
2023-06-26 19:29:54 -0400

    Update inference_server_example.py

--- inference_server_example.py
+++ inference_server_example.py
@@ -1,10 +1,10 @@
 # Inference server file should contain the single class named BaseServer,
 # with two methods defined:
-#   - setup(): sets up model
-#   - predict(): makes predictions
+#   - setup(): sets up your model;
+#   - predict(): makes predictions.
 class BaseServer:
     # Setup step happens once per inference server setup.
-    # All setup things like model load, pipeline setup, etc. should happens here:
+    # All setup things like model load, pipeline setup, etc. should happen here:
     def setup(self):
         class DummyModel:
             def predict(self, input: str) -> str:
@@ -12,7 +12,8 @@ class BaseServer:
         self._model = DummyModel()
 
     # Prediction step makes requests processing and prediction.
-    # It should take list of requests and return list of prediction results 
+    # It should take list of requests and return list of prediction results.
+    # Call your model/pipeline that your set up in setup() to get the predictions.
     def predict(self, request: List[str]) -> List[str]:
         results = []
         for request in request:
